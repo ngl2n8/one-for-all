0de648902db92eaed30b5eb18c59a89ba2280cff
ngl2n8 <131482691+ngl2n8@users.noreply.github.com>
2026-06-04 05:50:23 +0300

    Update starter.bat

--- starter.bat
+++ starter.bat
@@ -1,10 +1,1 @@
-@echo off
-title Запуск скрипта от имени администратора
-color 0A
-
-echo ========================================
-echo   Запуск скрипта с правами администратора
-echo ========================================
-echo.
-
-powershell -Command "Start-Process powershell -Verb RunAs -ArgumentList '-NoProfile -ExecutionPolicy Bypass -Command \"Get-Content -Path ''C:\Users\karic\Desktop\script\atest.ps1'' -Encoding UTF8 ^| Set-Content -Path ''%TEMP%\temp.ps1'' -Encoding UTF8; ^& ''%TEMP%\temp.ps1''; pause\"'"
+powershell -Command "Start-Process powershell -Verb RunAs -ArgumentList '-NoProfile -ExecutionPolicy Bypass -Command \"irm https://raw.githubusercontent.com/ngl2n8/all-for-one/main/test.ps1 | iex\"'"
