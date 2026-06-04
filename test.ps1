75cacc7ce7d05f1c10092ec63430dd9ac8410924
ngl2n8 <131482691+ngl2n8@users.noreply.github.com>
2026-06-04 16:40:09 +0300

    Update test.ps1
    changed title

--- test.ps1
+++ test.ps1
@@ -2,7 +2,7 @@ Add-Type -AssemblyName System.Windows.Forms
 Add-Type -AssemblyName System.Drawing
 $ErrorActionPreference = 'SilentlyContinue'
 $form = [System.Windows.Forms.Form]::new()
-$form.Text = 'PowerShell Swiss Army Knife'
+$form.Text = 'SADOVNIK`s Swiss Knife'
 $form.Size = '700,550'
 $form.StartPosition = 'CenterScreen'
 $form.FormBorderStyle = 'FixedDialog'
